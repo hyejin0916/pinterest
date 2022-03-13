ea5c24a5cfe83c00e317dddb16693df436cf8690
hyejin <rkdgpwlsz@naver.com>
2022-03-13 21:39:35 +0900

    last

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True) # set_null은 게시글이 사라졌을 때 게시글 삭제x
+
+    title = models.CharField(max_length=200, null=True)
+    content = models.TextField(null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+
+    created_at = models.DateField(auto_now_add=True, null=True)
