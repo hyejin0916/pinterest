090bc18112ec41e38b12d5e6c6c4221ffe4dded5
hyejin <rkdgpwlsz@naver.com>
2022-03-18 00:43:23 +0900

    36

--- repinterest/urls.py
+++ repinterest/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls'))
-] + static(settings.MEDIA_URL,document_root=settings.MEDIA_URL)
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
